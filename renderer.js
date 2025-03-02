fc473d61ad2cee0381911e0ac729dcdf8abea153
Tharun <tharungowda30115@gmail.com>
2025-03-02 14:45:56 +0530

    updates

--- renderer.js
+++ renderer.js
@@ -1,17 +1,15 @@
 window.electronAPI.fileOpened((event, data) => {
-	const textArea = document.getElementById("text-area");
-	textArea.value = data;
-})
-
+  const textArea = document.getElementById("text-area");
+  textArea.value = data;
+});
 
 // listen for ctrl+s or cmd+s and save the file
 
-
 document.addEventListener("keydown", async (event) => {
-	if ((event.ctrlKey || event.metaKey) && event.key === "s") {
-		event.preventDefault();
-		const textArea = document.getElementById("text-area");
-		const filePath = await window.electronAPI.saveFile(textArea.value);
-		if (filePath) alert(`File saved at ${filePath}`);
-	}
-})
+  if ((event.ctrlKey || event.metaKey) && event.key === "s") {
+    event.preventDefault();
+    const textArea = document.getElementById("text-area");
+    const filePath = await window.electronAPI.saveFile(textArea.value);
+    if (filePath) alert(`File saved at ${filePath}`);
+  }
+});
